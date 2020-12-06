245f1e07c87fae691363be93cea17fd64d21769d
srikar12345678 <75025524+srikar12345678@users.noreply.github.com>
2020-12-06 14:45:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var a,b;
+
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  a.shapeColor="green";
+  b=createSprite(200,100,30,30);
+  b.shapeColor="green"
+
+
+}
+
+function draw() {
+  a.x=World.mouseX;
+  a.y=World.mouseY;
+  background(255,255,255);  
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2 ){
+    a.shapeColor="red";
+    b.shapeColor="red";
+    
+  }
+  else{
+    a.shapeColor="green";
+    b.shapeColor="green";
+    
+  }
+  drawSprites();
+}
